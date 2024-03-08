908f48926f7f32e56eeed7a09a7b7ecf3d345dfd
mstsurnalyakter <surnalyms@gmail.com>
2024-03-08 10:57:42 +0600

    conditional rendering

--- react/reat1/practice/src/App.jsx
+++ react/reat1/practice/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Todo from './Todo';
+import Todo2 from './Todo2';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>Todo App 1</h3>
+      <Todo task="Learning React" isDone={true}></Todo>
+      <Todo task="Core concepts" isDone={false}></Todo>
+      <h3>Todo App 2</h3>
+      <Todo2 task="Learning JavaScript" isDone={true}></Todo2>
+      <Todo2 task="Learning NodeJS" isDone={false}></Todo2>
     </>
-  )
+  );
 }
 
 export default App
