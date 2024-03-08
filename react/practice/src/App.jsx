62692a8756701719925edd79690771fa4da88758
mstsurnalyakter <surnalyms@gmail.com>
2024-03-08 19:43:49 +0600

    added practice_project new react vite folder

---
+++ react/practice/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+
+import './App.css'
+import Gallery from './Gallery'
+
+function App() {
+
+  return (
+    <>
+      <Gallery></Gallery>
+    </>
+  )
+}
+
+export default App
---
+++ react/practice/src/Gallery.jsx
@@ -0,0 +1,10 @@
+import Profile from "./Profile";
+
+export default function Gallery(){
+    return(
+        <section>
+            <h3>Amazing Scientist</h3>
+            <Profile></Profile>
+        </section>
+    )
+}
