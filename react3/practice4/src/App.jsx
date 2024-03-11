9c8e98dd6878746b656bed31ebf9a6537b89e299
mstsurnalyakter <surnalyms@gmail.com>
2024-03-11 20:37:55 +0600

    added smart watch

--- react3/practice4/src/App.jsx
+++ react3/practice4/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
+import Sunglass from './Components/Sunglass/Sunglass'
+import Watch from './Components/Watch/Watch'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const [watches,setWatches] = useState([])
+useEffect(()=>{
+  const loadWatches = async ()=>{
+    const res = await fetch('watch.json');
+    const data = await res.json();
+    setWatches(data);
+  }
+  loadWatches();
+},[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        watches.map((watch)=><Watch key={watch.id} watch={watch} />)
+      }
     </>
   )
 }
